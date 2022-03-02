0f78e89bf5c4785de44c221f20fe1a7540f34f97
wagnerberna <cabelo29007@gmail.com>
2022-03-02 07:46:59 -0300

    deploy heroku

--- src/service/postgresql.py
+++ src/service/postgresql.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 POSTGRESQL_LOCALHOST = os.getenv('POSTGRESQL_LOCALHOST')
-POSTGRESSQL_HEROKU = os.getenv('POSTGRESSQL_HEROKU')
+POSTGRESQL_HEROKU = os.getenv('POSTGRESQL_HEROKU')
 
 
 # POSTGRESQL_LOCALHOST = "postgresql://postgres:123@localhost:5432/wb_db"
-# POSTGRESSQL_HEROKU = "postgres://ndxyrolnkefpxf:cc9f341cfcf4f8b076ee282ecb88872fe9578fa8c53ed495ff15ea6663178a8d@ec2-52-45-83-163.compute-1.amazonaws.com:5432/dbvld7vgp4pomg"
-# DATABASE_URL = os.environ['DATABASE_URL']
+# POSTGRESQL_HEROKU = "postgres://ndxyrolnkefpxf:cc9f341cfcf4f8b076ee282ecb88872fe9578fa8c53ed495ff15ea6663178a8d@ec2-52-45-83-163.compute-1.amazonaws.com:5432/dbvld7vgp4pomg"
+DATABASE_URL = os.environ['DATABASE_URL']
 
 
 class Postgresql:
     def postgresql_connect(self):
         try:
-            # connection = psycopg2.connect(POSTGRESSQL_HEROKU, sslmode='require')
-            connection = psycopg2.connect(POSTGRESSQL_HEROKU)
+            # connection = psycopg2.connect(POSTGRESQL_HEROKU, sslmode='require')
+            connection = psycopg2.connect(DATABASE_URL)
 
             return connection
 
